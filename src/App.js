27d925101e0d9c7b6dc5612ed15bbd128f9df47e
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-15 00:07:22 +0600

    add header with navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
@@ -10,6 +12,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../Assets/images/logo2.png';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <header className='d-flex px-5 mt-2 justify-content-between align-items-center'>
+            <div className="logo-section">
+                <img src={logo} alt="" />
+            </div>
+            <nav className='navbar'>
+                <Link to={'/'}>Cart</Link>
+                <Link to={'/login'}>Login</Link>
+                <Link className='btn btn-primary text-white rounded-pill px-4' to={'/signup'}>Sign up</Link>
+            </nav>
+        </header>
     );
 };
 
